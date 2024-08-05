43caf48d3bdef7e7599ee88464c5a95ae119637d
Edwilson <edwsilva@gmail.com>
2024-08-04 21:10:57 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+import { Link, Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Users from "./pages/Users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
+          <Nav className="me-auto">
+            <Nav.Link as="span">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </Nav.Link>
+            <Nav.Link as="span">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </Nav.Link>
+            <Nav.Link as="span">
+              <Link to="/users" className="nav-link">
+                Users
+              </Link>
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+    </Router>
   );
 }
 
