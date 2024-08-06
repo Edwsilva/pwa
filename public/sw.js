ff95778ed44b8fd3f793e19ad395a3dc05e9ac3c
Edwilson <edwsilva@gmail.com>
2024-08-06 19:14:00 -0300

    voltar a versão anterior

--- public/sw.js
+++ public/sw.js
@@ -16,56 +16,56 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// this.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((cachedResponse) => {
-//       console.log("cachedResponse $$$ ", cachedResponse);
-//       if (cachedResponse) {
-//         return cachedResponse;
-//       }
-//       return fetch(event.request)
-//         .then((networkResponse) => {
-//           console.log("cachedResponse no fetch $$$ ", cachedResponse);
-//           let responseClone = networkResponse.clone();
-//           if (networkResponse && networkResponse.status === 200) {
-//             caches.open(cacheData).then((cache) => {
-//               cache.put(event.request, responseClone);
-//             });
-//           }
-//           return networkResponse;
-//         })
-//         .catch(() => {
-//           // Aqui você pode fornecer uma resposta de fallback offline, se desejar
-//           console.log("catch offline");
-//           return caches.match("/index.html");
-//         });
-//     })
-//   );
-//   // }
-// });
-
-self.addEventListener("fetch", (event) => {
+this.addEventListener("fetch", (event) => {
   event.respondWith(
-    (async () => {
-      try {
-        // Check if we are online
-        const response = await fetch(event.request);
-        const cache = await caches.open(cacheData);
-        cache.put(event.request, response.clone());
-        return response;
-      } catch (error) {
-        // Offline, use cache if available
-        const cachedResponse = await caches.match(event.request);
-        if (cachedResponse) {
-          return cachedResponse;
-        } else {
-          // Optionally, handle the error or return a fallback
-          return new Response("Offline and no cache available.", {
-            status: 404,
-            statusText: "Not Found",
-          });
-        }
+    caches.match(event.request).then((cachedResponse) => {
+      console.log("cachedResponse $$$ ", cachedResponse);
+      if (cachedResponse) {
+        return cachedResponse;
       }
-    })()
+      return fetch(event.request)
+        .then((networkResponse) => {
+          console.log("cachedResponse no fetch $$$ ", cachedResponse);
+          let responseClone = networkResponse.clone();
+          if (networkResponse && networkResponse.status === 200) {
+            caches.open(cacheData).then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+          }
+          return networkResponse;
+        })
+        .catch(() => {
+          // Aqui você pode fornecer uma resposta de fallback offline, se desejar
+          console.log("catch offline");
+          return caches.match("/index.html");
+        });
+    })
   );
+  // }
 });
+
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     (async () => {
+//       try {
+//         // Check if we are online
+//         const response = await fetch(event.request);
+//         const cache = await caches.open(cacheData);
+//         cache.put(event.request, response.clone());
+//         return response;
+//       } catch (error) {
+//         // Offline, use cache if available
+//         const cachedResponse = await caches.match(event.request);
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         } else {
+//           // Optionally, handle the error or return a fallback
+//           return new Response("Offline and no cache available.", {
+//             status: 404,
+//             statusText: "Not Found",
+//           });
+//         }
+//       }
+//     })()
+//   );
+// });
